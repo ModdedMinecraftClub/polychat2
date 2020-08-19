e21b77ca80f19e0c0c6791d6f4acd1d67db81f60
john01dav <john01dav@gmail.com>
2020-08-19 08:44:38 -0500

    Fixed bug of missing method in networking library client

--- core/network-library/src/main/java/club/moddedminecraft/polychat/core/networklibrary/Client.java
+++ core/network-library/src/main/java/club/moddedminecraft/polychat/core/networklibrary/Client.java
@@ -56,6 +56,24 @@ public final class Client{
         return messages;
     }
 
+    /**
+     * Sends a message to the connected server
+     *
+     * @param message The message to send
+     * @throws IOException If there are networking errors and a new connection can't be established. If there is an
+     *                     error here, it is recommended that you print the exception to the console and call
+     *                     <code>poll()</code> again with the normal schedule.
+     */
+    public void sendMessage(byte[] message) throws IOException{
+        if(client == null){
+            reinitializeClient();
+        }
+
+        if(client != null){
+            client.sendMessage(message);
+        }
+    }
+
     private void reinitializeClient() throws IOException{
         client = null;
         long currentTime = System.currentTimeMillis();
