ff2404dd9a51c7eefe4b47de14a307587ec9a93e
132ikl <132@ikl.sh>
2020-08-21 01:07:50 -0400

    Add working base with dependencies for all clients

---
+++ client/bukkit-1.7.10/src/main/java/club/moddedminecraft/polychat/client/bukkit1710/Polychat.java
@@ -0,0 +1,17 @@
+package club.moddedminecraft.polychat.client.bukkit1710;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Polychat extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+    }
+
+
+}
