32e4dada7229c8bbfb56f1794c05af7b8a8c5a52
Trace <lukaszdabrowski881@gmail.com>
2020-08-28 14:05:07 +0100

    clean imports

--- server/src/main/java/club/moddedminecraft/polychat/core/server/handlers/PromoteMemberCommandHandler.java
+++ server/src/main/java/club/moddedminecraft/polychat/core/server/handlers/PromoteMemberCommandHandler.java
@@ -2,7 +2,6 @@ package club.moddedminecraft.polychat.core.server.handlers;
 
 import club.moddedminecraft.polychat.core.messagelibrary.CommandProtos;
 import club.moddedminecraft.polychat.core.networklibrary.ConnectedClient;
-import club.moddedminecraft.polychat.core.networklibrary.Message;
 import club.moddedminecraft.polychat.core.server.EventHandler;
 import club.moddedminecraft.polychat.core.server.OnlineServer;
 import com.google.protobuf.Any;
