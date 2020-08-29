d75af4dae5718f44963747e91edecd11a7853949
132ikl <132@ikl.sh>
2020-08-29 17:57:04 -0400

    Begin work on client base

---
+++ client/client-base/src/main/java/ClientBase.java
@@ -0,0 +1,5 @@
+public interface ClientBase {
+
+    void recieveMessage();
+
+}
---
+++ client/client-base/src/main/java/PolychatClient.java
@@ -0,0 +1,10 @@
+public class PolychatClient {
+
+    public PolychatClient(ClientBase impl) {
+
+    }
+
+    public void update() {
+
+    }
+}
