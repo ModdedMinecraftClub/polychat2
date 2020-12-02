7032b0420dfd959b7225b7f11222f91e9a02b65c
132ikl <132@ikl.sh>
2020-12-01 21:10:40 -0500

    Add debug catch to ChatMessageHandler

--- server/src/main/java/club/moddedminecraft/polychat/server/handlers/protomessages/ChatMessageHandler.java
+++ server/src/main/java/club/moddedminecraft/polychat/server/handlers/protomessages/ChatMessageHandler.java
@@ -5,6 +5,10 @@ import club.moddedminecraft.polychat.core.messagelibrary.EventHandler;
 import club.moddedminecraft.polychat.core.networklibrary.ConnectedClient;
 import club.moddedminecraft.polychat.server.OnlineServer;
 import com.google.protobuf.Any;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.util.HashMap;
@@ -12,6 +16,7 @@ import java.util.HashMap;
 public final class ChatMessageHandler {
     private final TextChannel generalChannel;
     private final HashMap<String, OnlineServer> onlineServers;
+    private final static Logger logger = LoggerFactory.getLogger(ChatMessageHandler.class);
 
     public ChatMessageHandler(TextChannel generalChannel, HashMap<String, OnlineServer> onlineServers) {
         this.generalChannel = generalChannel;
@@ -26,10 +31,21 @@ public final class ChatMessageHandler {
 
     private void sendMessageToDiscord(ChatProtos.ChatMessage msg) {
         String discordMsg = msg.getMessage().replaceAll("§.", "");
+        String part1;
+        String part2;
+        try {
+            part1 = discordMsg.substring(0, msg.getMessageOffset());
+            part2 = discordMsg.substring(msg.getMessageOffset());
+        } catch (StringIndexOutOfBoundsException e) {
+            logger.error("STRING OUT OF BOUNDS EXCEPTION");
+            logger.error("MESSAGE: " + msg.getMessage());
+            logger.error("OFFSET: " + msg.getMessageOffset());
+            return;
+        }
         discordMsg = "`"
-                + discordMsg.substring(0, msg.getMessageOffset()) +
+                + part1 +
                 "` "
-                + discordMsg.substring(msg.getMessageOffset());
+                + part2;
         generalChannel.sendMessage(discordMsg).queue();
     }
 
